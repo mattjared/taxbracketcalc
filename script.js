55269e1b8b8faa2d0c6f9080d61e54872980db9b
mattjared <mattjared9@gmail.com>
2015-02-28 17:30:55 -0600

    basic changes

--- script.js
+++ script.js
@@ -1,29 +1,36 @@
-var first = .1;
+var indyFirst = .1;
 
-function second(income) {
-  var secondTier = .15,
-  answer = (income * secondTier) + 995;
+function indySecond(income) {
+  var indySecondTier = .15,
+  answer = (income * indySecondTier) + 995;
  	$("p").text(answer);
 }
 
+function indyThird(income) {
+	var indyThirdTier = .25,
+	answer = (income * indyThirdTier) + 5156.25;
+	$("p").text(answer);
+}
+
 $('input:radio[name=filingType]').change(function(){
-	if (this.value == 'a') {
+	if (this.value == 'indy') {
  		$("#income")
       .keyup(function(){
     		var value = $(this).val();
         if (value <= 9225) {
-          $("p").text(value * first); 
+          $("p").text(value * indyFirst); 
         }
-        else if (value > 9225 && value < 37450) {
-          $("p").text(second(value));
+        else if (value > 9226 && value < 37450) {
+          $("p").text(indySecond(value));
         }
-      	else {
-          second(value);
+      	else if (value > 37451 && value < 90750 ) {
+          // $("p").text(indySecond(value));
+          $("p").text("Fuck");
         }
     	})
     	.keyup();
   }
-  else if (this.value == 'b'){
+  else if (this.value == 'married'){
     $("#income")
       .keyup(function(){
     		var value = $(this).val();
