85ce6571b11bc8a0b962b7d4e27fbf4e7f2221fd
mattjared <mattjared9@gmail.com>
2015-02-28 19:11:19 -0600

    Removing redundant jquery

--- script.js
+++ script.js
@@ -1,17 +1,3 @@
-var indyFirst = .1;
-
-function indySecond(income) {
-  var indySecondTier = .15,
-  answer = 922.50 + ((income - 995) * indySecondTier);
- 	$("p").text(answer);
-}
-
-function indyThird(income) {
-	var indyThirdTier = .25,
-	answer = 5156.25 + ((income - 37450) * indySecondTier);
-	$("p").text(answer);
-}
-
 $('input:radio[name=filingType]').change(function(){
 	if (this.value == 'indy') {
  		$("#income")
@@ -21,10 +7,10 @@ $('input:radio[name=filingType]').change(function(){
           $("p").text(value * indyFirst); 
         }
         else if (value > 9226 && value < 37450) {
-          $("p").text(indySecond(value));
+          indySecond(value);
         }
       	else if (value > 37451 && value < 90750 ) {
-          $("p").text(indyThird(value));
+          indyThird(value);
         }
     	})
     	.keyup();
@@ -43,3 +29,17 @@ $('input:radio[name=filingType]').change(function(){
     	.keyup();
   }
 });
+
+var indyFirst = .1;
+
+function indySecond(income) {
+  var indySecondTier = .15,
+  answer = 922.50 + ((income - 995) * indySecondTier);
+ 	$("p").text(answer);
+}
+
+function indyThird(income) {
+	var indyThirdTier = .25,
+	answer = 5156.25 + ((income - 37450) * indyThirdTier);
+	$("p").text(answer);
+}
